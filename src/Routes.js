d8a0b38e77bb5ed6e441306ca641eb3ec05b8662
Chad Schroeder <gmchad@gmail.com>
2019-03-21 15:24:39 -0700

    removed HomeContainer link

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import PostContainer from './containers/PostContainer';
 import NewPostContainer from './containers/NewPostContainer';
-import HomeContainer from './containers/HomeContainer';
+import Home from './components/Home/Home';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <HomeContainer />} />
+        <Route exact path="/" render={() => <Home />} />
         <Route
           exact
           path="/new"
