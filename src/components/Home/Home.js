3476aca4e52971b5c6beda9be71883da34bc939f
Chad Schroeder <gmchad@gmail.com>
2019-03-21 15:29:43 -0700

    removed PostCard component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
-import PostCard from '../PostCard/PostCard';
+import React, { PureComponent } from 'react';
+import TitleList from '../../containers/TitleList';
 import styled from 'styled-components';
 
-const uuid = require('uuid/v4');
-
-/* Styling */
-
-const Header = styled.header`
+const Welcome = styled.div`
   border-top: 4px solid #444;
 `;
 
@@ -24,39 +20,71 @@ const Title = styled.h2`
   font-weight: 600;
 `;
 
-class Home extends Component {
-  static defaultProps = {
-    posts: []
-  };
-
-  componentDidMount() {
-    this.props.getPostsFromAPI();
-  }
-
+class Home extends PureComponent {
   render() {
-    const posts = Object.values(this.props.posts).map(post => (
-      <li key={uuid()} className="list-group-item">
-        <PostCard {...post} />
-      </li>
-    ));
-
     return (
       <>
-        <Header className="mb-3 px-4 py-4 bg-dark text-white">
+        <Welcome className="mb-3 px-4 py-4 bg-dark text-white">
           <Lead>
             Welcome to <strong>Microblog</strong>, our innovative site for
             communicating on the information&nbsp;superhighway.
           </Lead>
-        </Header>
+        </Welcome>
         <Heading className="mb-0 py-3 px-4">
           <Title className="mb-0 h5">
             Posts
           </Title>
         </Heading>
-        <ul className="list-group list-group-flush">{posts}</ul>
+        <TitleList />
+        {/* <ul className="list-group list-group-flush">{posts}</ul> */}
       </>
     );
   }
 }
+
+
+// import PostCard from '../PostCard/PostCard';
+// import styled from 'styled-components';
+
+// const uuid = require('uuid/v4');
+
+
+
+
+
+// class Home extends Component {
+//   static defaultProps = {
+//     posts: []
+//   };
+
+//   componentDidMount() {
+//     this.props.getPostsFromAPI();
+//   }
+
+//   render() {
+//     const posts = Object.values(this.props.posts).map(post => (
+//       <li key={uuid()} className="list-group-item">
+//         <PostCard post={post} />
+//       </li>
+//     ));
+
+    // return (
+    //   <>
+    //     <Header className="mb-3 px-4 py-4 bg-dark text-white">
+    //       <Lead>
+    //         Welcome to <strong>Microblog</strong>, our innovative site for
+    //         communicating on the information&nbsp;superhighway.
+    //       </Lead>
+    //     </Header>
+    //     <Heading className="mb-0 py-3 px-4">
+    //       <Title className="mb-0 h5">
+    //         Posts
+    //       </Title>
+    //     </Heading>
+    //     <ul className="list-group list-group-flush">{posts}</ul>
+    //   </>
+    // );
+//   }
+// }
 
 export default Home;
